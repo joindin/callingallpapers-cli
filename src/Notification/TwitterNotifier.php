5787553dbac95a38c2b9a37eac41393dc835d92b
Andreas Heigl <andreas@heigl.org>
2016-09-23 09:42:51 +0200

    Adds Twitter-notification 24hours before CfP End
    To allow people being notified of ending CfPs

---
+++ src/Notification/TwitterNotifier.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright (c) Andreas Heigl<andreas@heigl.org>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright Andreas Heigl
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @since     22.09.2016
+ * @link      http://github.com/heiglandreas/callingallpapers
+ */
+
+namespace Callingallpapers\Notification;
+
+use Callingallpapers\Entity\Cfp;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
+class TwitterNotifier implements NotificationInterface
+{
+    protected $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function notify(Cfp $cfp)
+    {
+        $name = $this->shortenName($cfp->conferenceName);
+        $uri  = $this->shortenUri($cfp->uri);
+
+        $notificationString = sprintf(
+            '24 hours until the CfP for "%1$s" closes: %2$s',
+            $name,
+            $uri
+        );
+        $request = new Request('POST',
+            'statuses/update.json?' . $this->formUrlEncode([
+                'status'              => $notificationString,
+                'lat'                 => $cfp->latitude,
+                'long'                => $cfp->longitude,
+                'display_coordinates' => 'true',
+            ]));
+
+        try {
+            $this->client->send($request);
+        } catch (\Exception $e) {
+            //
+        }
+
+    }
+
+    protected function shortenName($name)
+    {
+        if (strlen($name) < 70) {
+            return $name;
+        }
+        return substr($name, 0, 69) . '…';
+    }
+
+    protected function shortenUri($uri)
+    {
+        return $uri;
+    }
+
+    protected function formUrlEncode(array $stuff)
+    {
+        return http_build_query($stuff);
+    }
+}
---
+++ src/Reader/ApiCfpReader.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Copyright (c) 2015-2016 Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright 2015-2016 Andreas Heigl/callingallpapers.com
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT-License
+ * @version   0.0
+ * @since     01.12.2015
+ * @link      http://github.com/heiglandreas/callingallpapers-cli
+ */
+namespace Callingallpapers\Reader;
+
+use Callingallpapers\Entity\Cfp;
+use Callingallpapers\Entity\CfpList;
+use GuzzleHttp\Client;
+
+class ApiCfpReader
+{
+    protected $baseUri;
+
+    protected $bearerToken;
+
+    protected $client;
+
+    public function __construct($baseUri, $bearerToken, $client = null)
+    {
+        $this->baseUri = str_Replace('/cfp', '', $baseUri);
+
+        $this->bearerToken = $bearerToken;
+        if (null === $client) {
+            $client = new Client([
+                'headers' => [
+                    'Accept' => 'application/json',
+                ]
+            ]);
+        }
+        $this->client = $client;
+    }
+
+    /**
+     * @param \DateInterval      $interval
+     * @param \DateTimeInterface $date
+     *
+     * @return CfpList
+     */
+    public function getCfpsEndingWithinInterval(\DateInterval $interval, \DateTimeInterface $date = null)
+    {
+        if (null === $date) {
+            $date = new \DateTimeImmutable();
+        }
+
+        $endDate = $date->add($interval);
+
+        $list = new CfpList();
+
+        try {
+            $result = $this->client->get(sprintf(
+                '%s/search?date_cfp_end[]=%s&date_cfp_end_compare[]=%s&date_cfp_end[]=%s&date_cfp_end_compare[]=%s',
+                $this->baseUri,
+                urlencode($date->setTimezone(new \DateTimeZone('UTC'))->format('c')),
+                urlencode('>'),
+                urlencode($endDate->setTimezone(new \DateTimeZone('UTC'))->format('c')),
+                urlencode('<')
+            ), [
+                'headers' => [
+                    'Accept' => 'application/json',
+                ]
+            ]);
+
+            $items = \GuzzleHttp\json_decode($result->getBody()->getContents(), true);
+
+        } catch (\Exception $e) {
+            return $list;
+        }
+
+        if (! $items) {
+            return $list;
+        }
+
+        foreach ($items['cfps'] as $item) {
+            $cfp = new Cfp();
+            $cfp->conferenceName = $item['name'];
+            $cfp->conferenceUri  = $item['eventUri'];
+            $cfp->dateEnd        = new \DateTimeImmutable($item['dateCfpEnd']);
+            $cfp->dateStart      = new \DateTimeImmutable($item['dateCfpStart']);
+            $cfp->eventDateEnd   = new \DateTimeImmutable($item['dateEventEnd']);
+            $cfp->eventDateStart = new \DateTimeImmutable($item['dateEventStart']);
+            $cfp->description    = $item['description'];
+            $cfp->location       = $item['location'];
+            $cfp->latitude       = $item['latitude'];
+            $cfp->longitude      = $item['longitude'];
+            $cfp->iconUri        = $item['iconUri'];
+            $cfp->uri            = $item['uri'];
+            $cfp->tags           = array_filter($item['tags'], function ($item) {
+                return (bool) $item;
+            });
+            $cfp->timezone       = $item['timezone'];
+
+            $tz = new \DateTimeZone($cfp->timezone);
+
+            $cfp->dateEnd->setTimezone($tz);
+            $cfp->dateStart->setTimezone($tz);
+            $cfp->eventDateEnd->setTimezone($tz);
+            $cfp->eventDateStart->setTimezone($tz);
+
+            $list->append($cfp);
+        }
+
+        return $list;
+    }
+}
