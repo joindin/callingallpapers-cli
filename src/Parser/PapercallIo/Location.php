3ccba83271180e38fbf631a57d949961436fbb11
Karl Hughes <khughes.me@gmail.com>
2019-06-19 14:19:30 -0500

    Fixing papercall parser, adding a little bit to the readme

--- src/Parser/PapercallIo/Location.php
+++ src/Parser/PapercallIo/Location.php
@@ -49,7 +49,7 @@ class Location implements EventDetailParserInterface
         }
 
         $location = trim($titlePath->item(0)->textContent);
-        $location = explode(' - ', $location);
+        $location = explode(PHP_EOL, $location);
         $cfp->location = $location[0];
 
         return $cfp;
